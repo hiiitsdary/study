7b7cba5ae262c5a68570ac02b25454cf8f93d0fb
Darya Kosolapova <kdasha126@gmail.com>
2021-04-18 23:40:19 +0400

    Search task in Array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,6 +2,17 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max = array[0]
+        for i in (0..array.length - 1)
+          if array[i] > max
+            max = array[i]
+          end
+        end
+        for i in (0..array.length - 1)
+          if array[i] > 0
+            array[i] = max
+          end
+        end
         array
       end
 
