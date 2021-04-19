011214699f953af572c8ef20aa6bff304a21592a
Darya Kosolapova <kdasha126@gmail.com>
2021-04-20 00:36:38 +0400

    Binary search task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -17,7 +17,23 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+        recursive_search(_array, _query, 0, _array.length)
+      end
+      
+      def recursive_search(_array, _query, left, right)
+        
+        middle = (left + right) / 2
+        if _array[middle] == _query
+          return middle
+        end
+        if (left - right).abs <= 1
+          return -1
+        end
+        if _array[middle] < _query
+          return recursive_search(_array, _query, middle, right)
+        else _array[middle] > _query
+          return recursive_search(_array, _query, left, middle)
+        end
       end
     end
   end
